d6187399f0f4f4db4e558838d36b8a0dbb62dc5e
Davidohis <davidi.ohis25@gmail.com>
2021-02-24 12:36:37 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Awaiken Theme">
+	<!-- Page Title -->
+	<title>Beauty & Spa HTML5 Bootstrap Free Template</title>
+	<!-- Google Fonts css-->
+	<link href="https://fonts.googleapis.com/css?family=Merienda+One%7cMontserrat:300,400,500,600,700,800,900" rel="stylesheet">
+	<!-- Bootstrap css -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<!-- Font Awesome icon css-->
+	<link href="css/font-awesome.min.css" rel="stylesheet" media="screen">
+	<link href="css/flaticon.css" rel="stylesheet" media="screen">
+	<!-- Slick nav css -->
+	<link rel="stylesheet" href="css/slicknav.css">
+	<!-- Main custom css -->
+	<link href="css/custom.css" rel="stylesheet" media="screen" >
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body data-spy="scroll" data-target="#navigation" data-offset="71">
+	<!-- Preloader starts -->
+	<div class="preloader">
+		<div class="browser-screen-loading-content">
+			<div class="loading-dots dark-gray">
+				<img src="images/hearts.svg" alt="Preloader" />
+			</div>
+		</div>
+    </div>
+	<!-- Preloader Ends -->
+	
+	<!-- Header Section Starts-->
+	<header>
+		<nav id="main-nav" class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<!-- Logo starts -->
+					<a class="navbar-brand" href="#">
+						<img src="images/small-logo.png" alt="Logo" />
+					</a>
+					<!-- Logo Ends -->
+					
+					<!-- Responsive Menu button starts -->
+					<div class="navbar-toggle">
+					</div>
+					<!-- Responsive Menu button Ends -->
+				</div>
+				
+				<div id="responsive-menu"></div>
+				
+				<!-- Navigation starts -->
+				<div class="navbar-collapse collapse" id="navigation">
+					<ul class="nav navbar-nav navbar-right main-navigation" id="main-menu">
+						<li class="active"><a href="/">Home</a></li>
+						<li><a href="/contact">Contact</a></li>
+					</ul>
+				</div>
+				<!-- Navigation Ends -->
+			</div>
+		</nav>
+	</header>
+	<!-- Header Section Ends-->
+	
+	<!-- Banner Section Starts -->
+	<div class="banner" id="home">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="header-banner">
+						<img src="images/logo.png" alt="Logo" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Banner Section Ends -->
+	
+	
+	<!-- Contact section starts -->
+	<section class="contact" id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="main-title">
+						<h2>Get in Touch</h2>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-12">
+					<div class="contact-box">
+						<div class="contact-info">
+							<p><i class="fa fa-mobile-phone"></i> 1-567-124-55555</p>
+							<p><i class="fa fa-map-marker"></i> 184 Main Street East Perl Habour 8007</p>
+							<p><i class="fa fa-clock-o"></i> Mon – Sat 8.00 – 18.00 Sunday CLOSED</p>
+						</div>
+						
+						<div class="contact-form">
+							<form>
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="ENTER YOUR NAME" />
+								</div>
+								
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="ENTER YOUR EMAIL" />
+								</div>
+								
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="ENTER YOUR MOBILE" />
+								</div>
+								
+								<div class="form-group">
+									<textarea class="form-control" rows="6" placeholder="ENTER MESSAGE HERE"></textarea>
+								</div>
+								
+								<div class="form-group">
+									<input type="submit" class="btn-custom" value="Submit" />
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Contact section ends -->
+	
+	<!-- Footer section starts -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="footer-logo">
+						<a href="#">
+							<i class="flaticon-lotus"></i>
+						</a>
+					</div>
+						
+					<div class="footer-social">
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-google-plus"></i></a>
+						<a href="#"><i class="fa fa-linkedin"></i></a>
+						<a href="#"><i class="fa fa-pinterest-p"></i></a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-6">
+					<div class="footer-info">
+						<p>Copyright &copy; Untitled. All rights reserved. Design By <a href="https://awaikenthemes.com/" target="_blank">Awaiken Theme</a> Images <a href="https://unsplash.com/" target="_blank">Unsplash</a>, <a href="https://pixabay.com/" target="_blank">Pixabay</a>, <a href="http://www.freepik.com" target="_blank">Freepik</a>, Icon <a href="https://www.flaticon.com/" target="_blank">Flaticon</a></p>
+					</div>
+				</div>
+				
+				<div class="col-md-6">
+					<div class="footer-menu">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Service</a></li>
+							<li><a href="#">Pricing</a></li>
+							<li><a href="#">Gallery</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- Footer section ends -->
+	
+    <!-- Jquery Library File -->
+	<script src="js/jquery-1.12.4.min.js"></script>
+	<!-- Parallax -->
+	<script src="js/jquery.parallax-1.1.3.js"></script>
+	<!-- SmoothScroll -->
+	<script src="js/SmoothScroll.js"></script>
+    <!-- Bootstrap js file -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Bootstrap form validator -->
+	<script src="js/validator.min.js"></script>
+    <!-- Counterup js file -->
+	<script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <!-- Isotop js file -->
+	<script src="js/isotope.min.js"></script>
+    <!-- Slick Nav js file -->
+	<script src="js/jquery.slicknav.js"></script>
+	<!-- Owl Carousel js file -->
+	<script src="js/owl.carousel.js"></script>
+    <!-- Typed js file -->
+	<script src="js/typed.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAsdzx6REuLqvQJC9VffvqM_84hUlKbWyM"></script> 
+    <!-- Main Custom js file -->
+    <script src="js/function.js"></script>
+</body>
+</html>
